0ef97b15487a684598e04ced82b0394ecf9a9e5c
StephenDGarcia <stephendgarcia@gmail.com>
2020-12-31 14:23:36 -0600

    fixed ``
    fixed my concatenation problem and everything prints

--- js/script.js
+++ js/script.js
@@ -12,31 +12,31 @@ project 1 - A Random Quote Generator
 ***/
 var quotes = [ 
   {
-    author: "Groucho Marx", 
+    source: "Groucho Marx", 
     quote: "I refuse to join any club that would have me as a member.",
     citation: '',
     year: ''
   },
   { 
-    author: "Will Ferrell",
+    source: "Will Ferrell",
     quote: "Before you marry a person, you should first make them use a computer with slow internet to see who they really are.",
     citation: '',
     year: '' 
   },
   { 
-    author: "Jerry Seinfeld",
+    source: "Jerry Seinfeld",
     quote: "Breaking up is like knocking over a Coke machine. You can't do it in one push; you got to rock it back and forth a few times, and then it goes over.",
     citation: '',
     year: '' 
   },
   { 
-    author: "Jimmy Kimmel", 
+    source: "Jimmy Kimmel", 
     quote: "I never feel more alone than when I'm trying to put sunscreen on my back.",
     citation: '',
     year: ''
   },
   { 
-    author: "Penny",
+    source: "Penny",
     quote: "My secret is so big that just the tip will be enough", 
     citation: "TV Show: Happy Endings", 
     year: "May 03, 2013"
@@ -65,12 +65,16 @@ function printQuote() {
   let html = ''; 
   html += `
    <p class="quote"> ${storedQuote.quote} </p>
-   <p class="source"> ${storedQuote.author} 
-    if (storedQuote.citation !== '') {
-    `<span> ${storedQuote.citation} </span>`; }
-    if (storedQuote.year !== '') {
-    `<span> ${storedQuote.year} </span>` }
-    </p>`
+   <p class="source"> ${storedQuote source}` 
+    
+   if (storedQuote.citation !== '') {
+    html += `<span> ${storedQuote.citation} </span>`; 
+   } 
+   if (storedQuote.year !== '') {
+    html += `<span> ${storedQuote.year} </span>`
+   }
+   
+   html += `</p>`
     ;
 
   document.getElementById('quote-box').innerHTML = html; 
